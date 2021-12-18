f3a5eef93b9cc705b16c8572fbc96d11a74202cc
silenc3502 <gcccompil3r@gmail.com>
2021-12-18 21:05:52 +0900

    Add Second Lecture

---
+++ 3기/LSH/c/2/for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAX				7
+
+int main(void)
+{
+	// C 표준의 경우엔 for문 내부에 int i형식으로 선언이 불가함
+	int i;
+
+	// 왜 7로 안하고 MAX를 적을까 ?
+	// 유지보수 차원에서 사용하는 것임(향후 확장성을 위함)
+	for (i = 0; i < MAX; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
