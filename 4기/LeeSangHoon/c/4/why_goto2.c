83d374ac768f2ccadfbc73707954dcdfff6cf2f8
silenc3502 <gcccompil3r@gmail.com>
2022-09-17 20:34:45 +0900

    4회차 - goto 빠우어

---
+++ 4기/LeeSangHoon/c/4/why_goto2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define ERROR			7
+#define LOOP_END		50
+
+int main(void)
+{
+	int i, j, k;
+	int data;
+	bool error_flag = false;
+
+	srand(time(NULL));
+
+	for (i = 0; i < LOOP_END; i++)
+	{
+		for (j = 0; j < LOOP_END; j++)
+		{
+			for (k = 0; k < LOOP_END; k++)
+			{
+				data = rand() % (LOOP_END + 1);
+				printf("data = %d\n", data);
+
+				if (data == ERROR) {
+					printf("Error 발생!\n");
+					error_flag = true;
+					break;
+				}
+			}
+
+			if (error_flag)
+			{
+				break;
+			}
+		}
+
+		if (error_flag)
+		{
+			break;
+		}
+	}
+
+	return 0;
+}
