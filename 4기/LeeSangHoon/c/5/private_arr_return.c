710d00c14247b712f172c3f87f13145b33fdc508
silenc3502 <gcccompil3r@gmail.com>
2022-09-24 20:49:13 +0900

    5회차 - private 구현

--- 4기/LeeSangHoon/c/5/private_arr_return.c
+++ 4기/LeeSangHoon/c/5/private_arr_return.c
@@ -14,11 +14,21 @@ int (* arr_return_test(void))[2]
 int main (void)
 {
 	int (*p)[2] = arr_return_test();
-	int i;
+	int i, j;
 
 	for (i = 0; i < 2; i++)
 	{
 		printf("*p[%d] = %d\n", i, *p[i]);
+	}
+
+	//arr[0][0] = 7;
+
+	for (i = 0; i < 2; i++)
+	{
+		for (j = 0; j < 2; j++)
+		{
+			printf("p[%d][%d] = %d\n", i, j, p[i][j]);
+		}
 	}
 
 	return 0;
