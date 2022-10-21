97e7db6133664e4c33243cab238e02cc055750bd
JinSeon627 <jinseon627@gmail.com>
2022-10-22 01:14:12 +0900

    유진선:DATA ACQUSITION2

--- 4기/YuJinSeon/c/7/data_acq_serv2.c
+++ 4기/YuJinSeon/c/7/data_acq_serv2.c
@@ -3,6 +3,8 @@
 
 typedef struct _power_grid_info power_grid_info;
 
+typedef struct _air_harmonic_info air_harmonic_info;
+
 #if 0
 struct _power_grid_info
 {
@@ -11,18 +13,38 @@ struct _power_grid_info
 };
 #endif
 
+#define POWER_GRID_DATA_SIZE	4
+
+
 struct _power_grid_info
 {
 	int data[0];
 };
 
-#define POWER_GRID_DATA_SIZE	4
+
+#define AIR_HARMONIC_DATA_SIZE	4
+
+
+struct _air_harmonic_info
+{
+	int data[0];
+};
+
 
 power_grid_info *init_power_grid_info(void)
 {
 	power_grid_info *tmp;
-
+	
 	tmp = (power_grid_info *)malloc(POWER_GRID_DATA_SIZE);
+	
+	return tmp;
+}
+
+air_harmonic_info *init_air_harmonic_info(void)
+{
+	air_harmonic_info *tmp;
+
+	tmp = (air_harmonic_info *)malloc(AIR_HARMONIC_DATA_SIZE);
 
 	return tmp;
 }
@@ -30,9 +52,11 @@ power_grid_info *init_power_grid_info(void)
 int main(void)
 {
 	power_grid_info *receive_power_data;
+	air_harmonic_info *receive_air_data;
 	printf("sizeof(power_grid_info) = %ld\n", sizeof(power_grid_info));
 
 	receive_power_data = init_power_grid_info();
+	receive_air_data = init_air_harmonic_info();
 
 	return 0;
 }
