94f50e4a0f1f06edcc34eb08dcbc58380890c69a
TryTwoTop <trytwotop@gmail.com>
2022-10-21 13:43:49 +0900

    B(공기조화) 단말기에서 서버로 전송되는 정보를 관리할 수 있는 구조체 설계 [DATA-ACQUISITION2]

--- 4기/JinDongMin/7_week/data_acq_serv.c
+++ 4기/JinDongMin/7_week/data_acq_serv.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define ALLOCATE_SUCCESS    0
 #define ALLOCATE_FAIL       -1
 
+// 전력 그리드의 전송 정보를 관리하는 구조체
 typedef struct _power_grid_info power_grid_info;
 struct _power_grid_info
 {
@@ -28,6 +28,34 @@ power_grid_info *init_power_grid_info(void)
 }
 
 void clear_power_grid_info(power_grid_info* tmp)
+{
+    free(tmp);
+    tmp = NULL;
+}
+
+// 공기조화장치의 전송 정보를 관리하는 구조체
+typedef struct _air_harmonic_info air_harmonic_info;
+struct _air_harmonic_info
+{
+    int data[0];
+};
+#define AIR_HARMONIC_DATA_SIZE  4
+
+air_harmonic_info *init_air_harmonic_info(void)
+{
+    air_harmonic_info *tmp;
+    tmp = (air_harmonic_info*)malloc(AIR_HARMONIC_DATA_SIZE);
+
+    if (tmp == NULL)
+    {
+        printf("air_harmonic_info 구조체 할당 실패\n");
+        exit(ALLOCATE_FAIL);
+    }
+
+    return tmp;
+}
+
+void clear_air_harmonic_info(air_harmonic_info* tmp)
 {
     free(tmp);
     tmp = NULL;
