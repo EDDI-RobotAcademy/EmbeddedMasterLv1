0922c13c222bb3bc5c3a39916a686a441e6613ec
TryTwoTop <trytwotop@gmail.com>
2022-10-27 20:50:07 +0900

    [진동민] 실습 파일 (5회차)

---
+++ 4기/JinDongMin/5_week/array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// 배열의 선언 방법
+	// 1. 데이터 타입 작성 (int, float, double 등등)
+	// 2. 변수 선언하듯이 이름 작성
+	// 3. 대괄호 열고 몇 개를 만들지 숫자를 작성
+	// 4. 필요하다면 { } 중괄호 내부에 배치할 값들을 작성
+
+	// 목적: 일괄적인 관리를 위해
+	int array[5] = { 1, 2, 3, 4 };
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("array[%d] = %d\n", i, array[i]);
+	}
+
+	return 0;
+}
---
+++ 4기/JinDongMin/5_week/pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 3;
+	int* p = &num;
+
+	*p = 7;
+
+	printf("num = %d\n", num);
+	printf("*p = %d\n", *p);
+
+	return 0;
+}
