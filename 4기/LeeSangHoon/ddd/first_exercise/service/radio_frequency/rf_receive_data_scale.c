77f92701b7c393c5463d035eb4580ab4a0d4b70c
silenc3502 <gcccompil3r@gmail.com>
2022-11-20 22:30:06 +0900

    feat: RF 신호를 DC Motor 처리에 적합한 형태로 스케일링하는 구현체 [DDD-FIRST6]

---
+++ 4기/LeeSangHoon/ddd/first_exercise/service/radio_frequency/rf_receive_data_scale.c
@@ -0,0 +1,62 @@
+#include "../../entity/radio_frequency/rf_receiver.h"
+#include "data_transfer_object/rf_receive_data_transfer_object.h"
+#include "rf_receive_data_scale.h"
+#include "../../common/common.h"
+
+int check_dc_motor_direction()
+{
+    if (rf_receive_dto.channel_data > 1020)
+        rf_receive_dto.channel_sub_order = CLOCK_WISE;
+    else if (rf_receive_dto.channel_data < 980)
+        rf_receive_dto.channel_sub_order = COUNTER_CLOCK_WISE;
+    else
+        rf_receive_dto.channel_sub_order = STOP;
+
+    return rf_receive_dto.channel_sub_order;
+}
+
+int clock_wise_dc_motor_scale (void)
+{
+    int range = RF_MAX - RF_MIDDLE;
+    float scale_factor = DC_MAX / (float)range;
+
+    return (rf_receive_dto.channel_data - RF_MIDDLE) * scale_factor;
+}
+
+int counter_clock_wise_dc_motor_scale (void)
+{
+    int range = RF_MIDDLE - RF_MIN;
+    float scale_factor = DC_MAX / (float)range;
+
+    return DC_MAX - (rf_receive_dto.channel_data - RF_MIN) * scale_factor;
+}
+
+int rf_scale_dc_motor (void)
+{
+    int scaled_channel_data;
+
+    if (check_dc_motor_direction() == CLOCK_WISE)
+    {
+        scaled_channel_data = clock_wise_dc_motor_scale();
+    }
+    else if (check_dc_motor_direction() == COUNTER_CLOCK_WISE)
+    {
+        scaled_channel_data = counter_clock_wise_dc_motor_scale();
+    }
+    else
+    {
+        scaled_channel_data = 0;
+    }
+
+    return scaled_channel_data;
+}
+
+int rf_scale_servo_motor (void)
+{
+    return 1;
+}
+
+int rf_scale_led (void)
+{
+    return 1;
+}
