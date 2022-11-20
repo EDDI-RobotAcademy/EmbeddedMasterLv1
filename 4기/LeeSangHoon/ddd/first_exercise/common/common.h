fbcd476ace761645f558fcdd2e9dfbf4a992dbc9
silenc3502 <gcccompil3r@gmail.com>
2022-11-20 21:46:10 +0900

    feat: RF 수신 정보를 일관된 방식으로 다루는 인터페이스 [DDD-FIRST5]

---
+++ 4기/LeeSangHoon/ddd/first_exercise/common/common.h
@@ -0,0 +1,20 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define DO_NOTHING              -1
+
+#define DC_MIN                  0
+#define DC_MAX                  255
+
+#define STOP                    0
+#define CLOCK_WISE              1
+#define COUNTER_CLOCK_WISE      2
+
+enum rf_scale_method {
+    RF_SCALE_DC_MOTOR,
+    RF_SCALE_SERVO_MOTOR,
+    RF_SCALE_LED,
+    RF_SCALE_END
+};
+
+#endif
