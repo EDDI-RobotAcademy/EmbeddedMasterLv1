23e6ce28a5bb5446dc6a626acdb6b6bed3004406
Sanghoon Lee <gcccompil3r@gmail.com>
2022-12-02 21:21:25 +0900

    feat: 사전 할당 메모리의 usage 상태 관리 초기화 [DDD-SECOND7]

---
+++ 4기/LeeSangHoon/ddd/second_exercise/domain/pre_allocated_memory/value_object/usage_memory_table.h
@@ -0,0 +1,26 @@
+#ifndef __USAGE_MEMORY_TABLE_H__
+#define __USAGE_MEMORY_TABLE_H__
+
+#include "../entity/pre_allocated_memory.h"
+
+typedef struct _usage_memory_idx usage_memory_idx;
+
+struct _usage_memory_idx
+{
+    int idx;
+    struct _usage_memory_idx *next;
+};
+
+typedef struct _usage_memory_table usage_memory_table;
+
+struct _usage_memory_table
+{
+    usage_memory_idx *usage_idx[MEMORY_SIZE_LIMIT];
+};
+
+usage_memory_table usage_memory[PREALLOC_COUNT];
+
+void init_usage_memory_table (void);
+void print_usage_memory_table (void);
+
+#endif
