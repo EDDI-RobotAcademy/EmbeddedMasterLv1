d139c68520b07389c6156c14f3ef17d4e86adb74
silenc3502 <gcccompil3r@gmail.com>
2022-12-04 12:04:24 +0900

    add: 내부 메모리 할당 누락 추가 [DDD-SECOND4]

--- 4기/LeeSangHoon/ddd/second_exercise/domain/pre_allocated_memory/entity/pre_allocated_memory.c
+++ 4기/LeeSangHoon/ddd/second_exercise/domain/pre_allocated_memory/entity/pre_allocated_memory.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 
 #include "pre_allocated_memory.h"
+#include "../value_object/free_memory_table.h"
 
 void init_pre_allocated_memory_0arg (void)
 {
@@ -18,7 +19,7 @@ void init_pre_allocated_memory_1arg (int allocated_max_memory_size)
 
 void alloc_pre_allocated_memory (void)
 {
-    int i;
+    int i, j;
     int idx = (enum slab_idx)SLAB_32;
 
     pre_allocated_memory.allocated_memory = (void ***)malloc(sizeof(void **) * PREALLOC_COUNT);
@@ -38,6 +39,16 @@ void alloc_pre_allocated_memory (void)
             return;
         }
 
+        for (j = 0; j < pre_allocated_memory.allocated_max_memory_size; j++)
+        {
+            pre_allocated_memory.allocated_memory[i][j] = malloc(sizeof(1 << idx));
+            if (pre_allocated_memory.allocated_memory[i][j] == NULL)
+            {
+                printf("Pre Allocated Memory 할당 실패!\n");
+                return;
+            }
+        }
+
         idx = idx << 1;
     }
 }
@@ -49,22 +60,3 @@ void set_pre_allocated_max_memory_size (int request_max_memory_size)
     pre_allocated_memory.allocated_max_memory_size = request_max_memory_size;
 }
 
-#define DOUBLE_PRESENTATION_WITH_INTEGER                2
-#define EXPONENT_IDX                                    1
-#define DOUBLE_PREICISION_BIAS                          1023
-#define FOCUS_EXPONENT                                  20
-
-int find_fit_slab_index(int request_memory_size)
-{
-    int exponent_number;
-    union
-    {
-        double ieee754_double_value;
-        int trick_shift_system[DOUBLE_PRESENTATION_WITH_INTEGER];
-    }
-    ieee754;
-
-    ieee754.ieee754_double_value = request_memory_size;
-    exponent_number = (ieee754.trick_shift_system[EXPONENT_IDX] >> FOCUS_EXPONENT) - DOUBLE_PREICISION_BIAS;
-    return exponent_number + ((request_memory_size & (request_memory_size - (1 << exponent_number))) ? 1 : 0);
-}
