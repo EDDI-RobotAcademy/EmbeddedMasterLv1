788e3a590281ec4aa8a9fecfb6d1deb6a662fe54
김영중 <doogidoogi@gmail.com>
2022-12-06 12:51:44 +0900

    김영중:[DDD_EXCERCISE1_RETRY2]

---
+++ 4기/KimYoungJoong/c/ddd_excercise1/rf_receiver/rf_receiver.c
@@ -0,0 +1,22 @@
+#include "rf_receiver.h"
+
+void rf_ch_set(void)
+{
+    rf_receive_object *rf_r_handler_ch1;
+    rf_receive_object *rf_r_handler_ch2;
+    rf_receive_object *rf_r_handler_ch3;
+    
+    rf_r_handler_ch1 = (rf_receive_object*) malloc(sizeof(rf_receive_object));
+    rf_r_handler_ch2 = (rf_receive_object*) malloc(sizeof(rf_receive_object));
+    rf_r_handler_ch3 = (rf_receive_object*) malloc(sizeof(rf_receive_object));
+
+}
+void rf_receive_signal(rf_receive_object *rf_r_handler)
+{
+    rf_r_handler->rf_receive_signal
+}
+
+void rf_receive_data_print(data_transfer_object *dto)
+{
+
+}
