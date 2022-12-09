5e226102675c4378feccb4a7605a28e3dba81324
김영중 <doogidoogi@gmail.com>
2022-12-09 11:17:04 +0900

    김영중:지정된 언어모드가 호환되지 않습니다.

---
+++ 4기/KimYoungJoong/c/ddd2_에러발생/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ecosense/EmbeddedMasterLv1/4기/KimYoungJoong/c/ddd2_member
+# Build directory: /home/ecosense/EmbeddedMasterLv1/4기/KimYoungJoong/c/ddd2_member
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ 4기/KimYoungJoong/c/ddd2_에러발생/utility/menu/menu.h
@@ -0,0 +1,17 @@
+#ifndef __MENU_H_
+#define __MENU_H_
+
+#define MENU_MIN    1
+#define MENU_MAX    5
+
+void proc_member_join(void);
+void proc_make_folder(void);
+void proc_make_file(void);
+void proc_view_file(void);
+void proc_all_member_print(void);
+
+
+
+
+
+#endif
