4250ca9e840aa976696b220905851a2d099e6a3a
TryTwoTop <trytwotop@gmail.com>
2023-01-14 11:53:24 +0900

    fix: [진동민] 헤더파일 중복삽입을 방지하기 위한 조건부 매크로 정의 [DDD-FIRST3]

--- 4기/JinDongMin/task/DDD_problem/1/utility/custom_random_int/custom_random_int.h
+++ 4기/JinDongMin/task/DDD_problem/1/utility/custom_random_int/custom_random_int.h
@@ -1,5 +1,10 @@
+#ifndef __CUSTOM_RANDOM_INT_H__
+#define __CUSTOM_RANDOM_INT_H__
+
 #include <stdlib.h>
 #include <time.h>
 
 void initialize_random(void);
 int generate_random_int_within_specific_range(int, int);
+
+#endif // __CUSTOM_RANDOM_INT_H__
