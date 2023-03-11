04da5be3641cfe6c349658bd3b8728d7ba4cfa0d
TryTwoTop <trytwotop@gmail.com>
2023-03-11 12:25:18 +0900

    add: [진동민] LED 상태 정보를 저장하고 변경할 수 있는 Entity 설계 [DDD-LED2]

--- 4기/JinDongMin/task/DDD_problem/firmware/led/led/entity/led.h
+++ 4기/JinDongMin/task/DDD_problem/firmware/led/led/entity/led.h
@@ -1,6 +1,8 @@
 #ifndef __LED_H__
 #define __LED_H__
 
+#include "../../pin/pin.h"
+
 enum LED_STATUS {
     LED_OFF,
     LED_ON
@@ -9,9 +11,11 @@ enum LED_STATUS {
 typedef struct _led_object led_object;
 
 struct _led_object {
+    pin *pin_info_ptr;
     int status;
 };
 
+void init_led(led_object *, pin *);
 void change_led_status(led_object *, int);
 
 #endif // __LED_H__
