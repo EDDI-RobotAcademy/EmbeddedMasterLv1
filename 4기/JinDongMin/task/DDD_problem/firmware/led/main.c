56efd401c7a329fb39c1ee8135a1b6fc7d68b0ca
TryTwoTop <trytwotop@gmail.com>
2023-03-18 12:19:40 +0900

    feat: LED 제어 동작 검증 [DDD-LED6]

--- 4기/JinDongMin/task/DDD_problem/firmware/led/main.c
+++ 4기/JinDongMin/task/DDD_problem/firmware/led/main.c
@@ -1,10 +1,27 @@
 #include <avr/io.h>
+#include <util/delay.h>
+
 #define F_CPU 16000000UL
+
+#include "pin/pin.h"
+#include "led/entity/led.h"
+#include "common/peripheral_table.h"
+
+pin pin_of_pb5;
+led_object led1;
 
 int main(void)
 {
+    init_pin(&pin_of_pb5, &PORTB, PORTB5);
+    init_led(&led1, &pin_of_pb5);
+
     while(1)
-    ;
+    {
+        pheripheral_call_table[LED_ON](&led1);
+        _delay_ms(500);
+        pheripheral_call_table[LED_OFF](&led1);
+        _delay_ms(500);
+    }
 
     return 0;
 }
