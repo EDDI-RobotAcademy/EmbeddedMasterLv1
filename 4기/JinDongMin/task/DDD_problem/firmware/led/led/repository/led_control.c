c7d1f039c725f14bd6fbf87e3f7b3612ead5fb73
TryTwoTop <trytwotop@gmail.com>
2023-03-25 11:48:16 +0900

    fix: LED를 끄는 함수의 버그 수정 [DDD-LED5]

--- 4기/JinDongMin/task/DDD_problem/firmware/led/led/repository/led_control.c
+++ 4기/JinDongMin/task/DDD_problem/firmware/led/led/repository/led_control.c
@@ -1,24 +1,20 @@
 #include "../service/led_control.h"
 
-void turn_on_led(led_object *led_object_ptr)
+void led_turn_on(led_object *led_object_ptr)
 {
     const pin *pin_ptr = led_object_ptr->pin_info_ptr;
 
     *(pin_ptr->port_register_ptr) |= _BV(pin_ptr->bit_number);
-
-    change_led_status(led_object_ptr, LED_ON);
 }
 
-void turn_off_led(led_object *led_object_ptr)
+void led_turn_off(led_object *led_object_ptr)
 {
     const pin *pin_ptr = led_object_ptr->pin_info_ptr;
 
-    *(pin_ptr->port_register_ptr) &= _BV(pin_ptr->bit_number);
-
-    change_led_status(led_object_ptr, LED_OFF);
+    *(pin_ptr->port_register_ptr) &= ~(_BV(pin_ptr->bit_number));
 }
 
-void toggle_led(led_object *led_object_ptr)
+void led_toggle(led_object *led_object_ptr)
 {
     switch (led_object_ptr->status)
     {
