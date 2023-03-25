f3cf83801f1f0cbf29ed976dc74cc056ff8013d5
TryTwoTop <trytwotop@gmail.com>
2023-03-25 11:04:37 +0900

    add: LED의 상태 정보를 나타내는 열거형과 받아올 수 있는 함수 추가 [DDD-LED2]

--- 4기/JinDongMin/task/DDD_problem/firmware/led/led/entity/led.h
+++ 4기/JinDongMin/task/DDD_problem/firmware/led/led/entity/led.h
@@ -3,19 +3,23 @@
 
 #include "../../pin/pin.h"
 
-enum LED_STATUS {
+typedef enum _LED_STATUS LED_STATUS;
+
+enum _LED_STATUS {
     LED_OFF,
-    LED_ON
+    LED_ON,
+    LED_END
 };
 
 typedef struct _led_object led_object;
 
 struct _led_object {
     pin *pin_info_ptr;
-    int status;
+    LED_STATUS status;
 };
 
-void init_led(led_object *, pin *);
-void change_led_status(led_object *, int);
+void init_led(led_object *, const pin *);
+void set_led_status(led_object *, LED_STATUS);
+LED_STATUS get_led_status(const led_object *);
 
 #endif // __LED_H__
