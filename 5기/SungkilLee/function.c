15b7b7e1685afc0b12d21cd00c91dc61f91d23b6
Sungkil Lee <sungkillee@Sungkilui-Macmini.local>
2023-05-24 23:25:01 +0900

    test

---
+++ 5기/SungkilLee/function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int multiply_two(int num)
+{
+	return num*2;
+}
+
+int main(void)
+{
+	int num = 3;
+	int result = multiply_two(num);
+	printf("result = %d\n",result);
+
+	return 0;
+}
+
