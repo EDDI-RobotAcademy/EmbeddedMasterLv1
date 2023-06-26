bdcad9f446970c4f2079eeabdd6a1cf3d0c67786
ske1222 <ske1222@naver.com>
2023-06-26 00:13:48 -0700

    feat : [Sang Ho Park] DDD + Xp 예제 프로젝트 Build 결과물

---
+++ 5기/SanghoPark/5주차/INTERFACE/build/CMakeFiles/interface.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/main/add_on_lib.c" "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/build/CMakeFiles/interface.dir/main/add_on_lib.c.o"
+  "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/main/main.c" "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/build/CMakeFiles/interface.dir/main/main.c.o"
+  "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/protocol/protocol_handler.c" "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/build/CMakeFiles/interface.dir/protocol/protocol_handler.c.o"
+  "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/receiver/receiver.c" "/home/psh/repo/academy/EmbeddedMasterLv1/5기/SanghoPark/5주차/INTERFACE/build/CMakeFiles/interface.dir/receiver/receiver.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
