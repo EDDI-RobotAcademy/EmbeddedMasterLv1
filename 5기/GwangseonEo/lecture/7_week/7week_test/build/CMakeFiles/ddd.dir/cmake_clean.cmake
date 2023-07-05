5c9bdec801c0adb3b5edfde5dbd839669b54e37f
oqazett <oqazett@gmail.com>
2023-07-05 12:53:12 +0900

    feat:[어광선] 6주차 강의 자료 정리

---
+++ 5기/GwangseonEo/lecture/7_week/7week_test/build/CMakeFiles/ddd.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ddd.dir/main/how_to_malloc.c.o"
+  "CMakeFiles/ddd.dir/main/how_to_malloc.c.o.d"
+  "CMakeFiles/ddd.dir/main/main.c.o"
+  "CMakeFiles/ddd.dir/main/main.c.o.d"
+  "ddd"
+  "ddd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ddd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 5기/GwangseonEo/lecture/7_week/7week_test/build/CMakeFiles/interface.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interface.dir/main/how_to_malloc.c.o"
+  "CMakeFiles/interface.dir/main/how_to_malloc.c.o.d"
+  "CMakeFiles/interface.dir/main/main.c.o"
+  "CMakeFiles/interface.dir/main/main.c.o.d"
+  "interface"
+  "interface.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
