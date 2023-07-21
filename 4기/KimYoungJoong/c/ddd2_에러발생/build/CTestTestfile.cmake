5fbf8eb6cd4a3fa2c1f8e83c6f33dabd5cbf841b
Sanghoon Lee <gcccompil3r@gmail.com>
2023-07-21 10:03:11 +0900

    chore: 4기 쓸모없는 파일들 정리

--- 4기/KimYoungJoong/c/ddd2_에러발생/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ecosense/EmbeddedMasterLv1/4기/KimYoungJoong/c/ddd2_member
-# Build directory: /home/ecosense/EmbeddedMasterLv1/4기/KimYoungJoong/c/ddd2_member/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
